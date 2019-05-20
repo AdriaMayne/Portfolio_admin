580f8e9d75d260ee4f291f6f201693549593bfd7
AdriaMayne <97.martimez@gmail.com>
2019-05-20 11:00:32 +0200

    Projects Create + Edit

---
+++ resources/views/admin/projects/new.blade.php
@@ -0,0 +1,62 @@
+@extends('templates.main')
+
+@section('own_css')
+    <link href="{{ asset('css/crud.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+    <div class="container">
+        <h4 class="text-center my-4 my-md-5">Nuevo PROYECTO</h4>
+        @include('partial.errores')
+        <form action="{{ action('ProjectController@store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="title">Título *</label>
+                    <input type="text" name="title" id="title" class="form-control col-12" placeholder="Introduce el título" value="{{ old('title') }}" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="name">URL *</label>
+                    <input type="text" name="url" id="url" class="form-control col-12" placeholder="Introduce la URL" value="{{ old('url') }}" required>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <label for="image">Imagen *</label>
+                    <div class="row mb-3">
+                        <a class="mx-auto" data-toggle="modal" data-target="#modalTestNew">
+                            <img id="thumbnail" class="img-thumbnail mx-auto" src="{{ asset('media/img/default_image.png') }}">
+                        </a>
+                    </div>
+                    <div class="custom-file">
+                        <input type="file" name="image" id="image" class="custom-file-input" value="{{ old('image') }}" required>
+                        <label class="custom-file-label" for="image">Seleccionar Archivo</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-12 mt-2">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="visible" name="visible" value="1" checked>
+                        <label class="custom-control-label" for="visible">Visible</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-12 mb-2 mt-2">
+                    <a href="{{ url('/projects') }}" class="btn btn-secondary btn-lg"><i class="fas fa-arrow-left mr-2"></i> Volver</a>
+                    <button type="submit" class="btn btn-primary btn-lg"><i class="fas fa-plus mr-2"></i> Añadir</button>
+                </div>
+            </div>
+            <small class="mx-0 p-0">Los campos indicados con * son obligatorios.</small>
+        </form>
+        <div class="modal fade" id="modalTestNew" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('js/events.js') }}"></script>
+    <script src="{{ asset('js/modals.js') }}"></script>
+@endsection
