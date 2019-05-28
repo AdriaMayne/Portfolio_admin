f3010c8c3b7f0db27629c483715f7118f436fa48
AdriaMayne <97.martimez@gmail.com>
2019-05-28 15:51:29 +0200

    Video Upgrade

--- public/js/video.js
+++ public/js/video.js
@@ -5,23 +5,52 @@ $(function() {
     var Adami_logo = $('#Adami_logo');
     var Petlinks_logo = $('#Petlinks_logo');
 
+    var AdriaMayneStart = 0;
+    var CookingMateStart = 5;
+    var AdamiStart = 16;
+    var PetlinksStart = 32;
+
     AdriaMayne_logo.on('click', function() {
-        video.currentTime = 0;
+        video.currentTime = AdriaMayneStart;
         video.play();
     });
 
     CookingMate_logo.on('click', function() {
-        video.currentTime = 5;
+        video.currentTime = CookingMateStart;
         video.play();
     });
 
     Adami_logo.on('click', function() {
-        video.currentTime = 16;
+        video.currentTime = AdamiStart;
         video.play();
     });
 
     Petlinks_logo.on('click', function() {
-        video.currentTime = 32;
+        video.currentTime = PetlinksStart;
         video.play();
+    });
+
+    function between(x, min, max) {
+        return x >= min && x < max;
+    }
+
+    $('#video').on('timeupdate', function() {
+        var current = video.currentTime;
+
+        if (between(current, AdriaMayneStart, CookingMateStart)) {
+            $('.images *').removeClass('highlight');
+            AdriaMayne_logo.addClass('highlight');
+        } else if (between(current, CookingMateStart, AdamiStart)) {
+            $('.images *').removeClass('highlight');
+            CookingMate_logo.addClass('highlight');
+        } else if (between(current, AdamiStart, PetlinksStart)) {
+            $('.images *').removeClass('highlight');
+            Adami_logo.addClass('highlight');
+        } else if (between(current, PetlinksStart, video.duration)) {
+            $('.images *').removeClass('highlight');
+            Petlinks_logo.addClass('highlight');
+        } else {
+            $('.images *').removeClass('highlight');
+        }
     });
 });
